7a97601de8199f5b45f81248bfb76137ed88c0e9
HarisSistek <ha.sistek@gmail.com>
2016-04-16 17:54:32 +0200

    Add new exercise function is working

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,10 +4,16 @@ angular.module('workout.controllers', [])
 
   console.log("In menu ctrl");
 
-  $scope.workouts = [
-    { title: "Workout A", id: 1},
-    { title: "Workout B", id: 2},
-    { title: "Workout C", id: 3}
+  $scope.workouts = [{
+      title: "Workout A",
+      id: 1
+    }, {
+      title: "Workout B",
+      id: 2
+    }, {
+      title: "Workout C",
+      id: 3
+    }
 
   ];
 
@@ -16,14 +22,50 @@ angular.module('workout.controllers', [])
   };
 })
 
-.controller('WorkoutCtrl', function($scope) {
+.controller('WorkoutCtrl', function($scope, $ionicModal) {
 
   console.log("In workout ctrl");
-
-  $scope.exercises = [
-    { title: "Exercise A", id: 1},
-    { title: "Exercise B", id: 2},
-    { title: "Exercise C", id: 3}
+  $scope.id = 4;
+  $scope.exercises = [{
+      title: "Exercise A",
+      id: 1
+    }, {
+      title: "Exercise B",
+      id: 2
+    }, {
+      title: "Exercise C",
+      id: 3
+    }
 
   ];
+
+  // Create and load the Modal
+  $ionicModal.fromTemplateUrl('templates/new_exercise.html', function(modal) {
+    $scope.exerciseModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  // Called when the form is submitted
+  $scope.createExercise = function(exe) {
+    $scope.exercises.push({
+      title: exe.title,
+      id: $scope.id
+    });
+    $scope.exerciseModal.hide();
+    exe.title = "";
+    $scope.id += 1;
+  };
+
+  // Open our new task modal
+  $scope.newExercise = function() {
+    $scope.exerciseModal.show();
+  };
+
+  // Close the new task modal
+  $scope.closeNewExercise = function() {
+    $scope.exerciseModal.hide();
+  };
+
 });
